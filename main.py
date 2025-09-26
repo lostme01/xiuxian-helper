14e82e7a18c6f86852e70bd158d6239efb98ba61
lostme01 <453789703@qq.com>
2025-09-27 00:24:14 +0800

    Auto update 2025-09-27 00:24:14

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
         # 运行主应用
         asyncio.run(app.run())
     except KeyboardInterrupt:
-        # 当用户按下 Ctrl+C 时，asyncio.run 会抛出 KeyboardInterrupt
-        # 我们在这里捕获它，但不做任何事 (pass)
-        # 因为 app.run() 的 finally 块已经处理了关闭日志
-        # 这样程序就会安静地退出，不会再打印长长的错误信息
+        # 当用户按下 Ctrl+C 时，程序会安静地退出
+        pass
+    # *** 修复：捕获 sys.exit() 抛出的 SystemExit 异常 ***
+    # 这样，当执行 `,重启` 时，程序也能安静地退出，不再打印长长的错误信息
+    except SystemExit:
         pass
